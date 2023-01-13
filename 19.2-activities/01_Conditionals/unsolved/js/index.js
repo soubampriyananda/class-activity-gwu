08f1c106243e37e380bcd78d7caaa41471852cd8
soubampriyananda <soubam@gmail.com>
2023-01-12 23:48:56 -0500

    Module 16

--- 19.2-activities/01_Conditionals/unsolved/js/index.js
+++ 19.2-activities/01_Conditionals/unsolved/js/index.js
@@ -1,3 +1,16 @@
 $('.tab').on("click", function(){
+
+  if ($(this).hasClass("tab1")) {
+    $(".tab1").addClass("active");
+    $(".tab2").removeClass("active");
+    $("#tabcontent1").css("display","block");
+    $("#tabcontent2").css("display","none");
+  }
   
+   else if ($(this).hasClass("tab2")) {
+      $(".tab1").addClass("active");
+      $(".tab2").removeClass("active");
+      $("#tabcontent1").css("display","none");
+    $("#tabcontent2").css("display","block");
+}
 });
---
+++ 19.2-activities/02_theme_changer/unsolved/js/themeChange.js
@@ -0,0 +1,14 @@
+$(".themeChange").on("click", function () {
+
+    if ($(this).hasClass("regular")) {
+        $("#theme").attr("href","css/index.css")
+    }
+   else if ($(this).hasClass("blue")) {
+        $("#theme").attr("href","css/blue.css")
+    }
+    else if ($(this).hasClass("dark")) {
+        $("#theme").attr("href","css/dark.css")
+    }
+
+
+});
